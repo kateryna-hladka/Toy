ad27fe7581c88e6f828fdcf748c4a52613c28316
Kateryna Hladka <katerynahladka1@gmail.com>
2025-01-23 19:38:07 +0200

    Refactoring

--- Toy/Utilit/StaticVariables.cs
+++ Toy/Utilit/StaticVariables.cs
@@ -3,5 +3,9 @@
     public static class StaticVariables
     {
         public static readonly string defaultPicture = "sign/Image-not-found.png";
+        public static readonly string regEmail = "^[\\w\\.\\-]+@[\\w\\-]+\\.[\\w\\-]{2,4}$";
+        public static readonly string regPhone = "^\\+?(\\(\\d{1,4}\\))?[\\d\\-]{1,13}$";
+        public static readonly string contactInfo = "Введіть пошту або номер телефону";
+        public static readonly string passwordInfo = "Пароль має бути від 4 до 8 символів";
     }
 }
